6d40844749b5a2b13ac5c1e3d2a2ba2e358c858f
Mo Mahboobian <momahboobian@gmail.com>
2024-06-26 12:11:03 +0100

    initialzing an app

--- 01-intro/solutions/exercise4.ts
+++
@@ -1,9 +0,0 @@
-let unknownValue: unknown = "This is a string... or is it?";
-
-if (typeof unknownValue === "string") {
-  console.log(`String value: ${unknownValue}`);
-}
-
-if (typeof unknownValue === "number") {
-  console.log(`Number value: ${unknownValue}`);
-}
