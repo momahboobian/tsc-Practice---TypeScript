5b87ae20e043d3293ab30c5844edf77645086f5f
Mo Mahboobian <momahboobian@gmail.com>
2024-06-26 13:07:05 +0100

    Excercise done

---
+++ Day-01/ts-nod-modules/dist/main.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const mathFunctions_1 = require("./mathFunctions");
+console.log("Addition: ", (0, mathFunctions_1.add)(5, 3));
+console.log("Subtraction: ", (0, mathFunctions_1.subtract)(5, 3));
+console.log("multiplication: ", (0, mathFunctions_1.subtract)(5, 3));
+console.log("division: ", (0, mathFunctions_1.subtract)(5, 3));
